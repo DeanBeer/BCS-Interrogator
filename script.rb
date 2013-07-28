9039eda93941ee58bef982c06ce774aa498fc70c
Dean Brundage <github@deanandadie.net>
2013-07-28 15:33:06 -0500

    removing

--- script.rb
+++
@@ -1,5 +0,0 @@
-$:.push File.expand_path('../lib', __FILE__)
-require 'bcs_interrogator'
-bcs = NRB::BCSInterrogator.new api_args: { base_url: 'http://beta.newrepublicbrewing.com:11405' }
-sysname = bcs.api.sysname
-data = sysname.data
